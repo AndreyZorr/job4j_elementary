71043dee5a4532d344a6713ef57b95bc685de67e
Andrey <AlpatoV_A_A@mail.ru>
2022-08-23 21:49:38 +0300

    1.1. Что такое import [#504773 #335363]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -4,8 +4,9 @@ import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
 
-    public static double sumOfTwo(double difference, double division) {
-        return sum(difference, division);
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
     }
 
     public static double differenceAndDivision(double third, double fourth) {
@@ -13,16 +14,20 @@ public class MathCalculator {
                 - division(third, fourth);
     }
 
-    public static double sumAndMultiply(double first, double second) {
-        return sum(first, second)
-                + multiply(first, second);
+    public static double sumOfTwo(double difference, double division) {
+        return difference + division;
+    }
+
+    public static double all(double multiply, double difference, double division, double sum) {
+        return multiply + difference + division + sum;
     }
 
     public static void main(String[] args) {
-        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета №1 равен: " + sumAndMultiply(10, 20));
         System.out.println("Результат расчета №2 равен: " + differenceAndDivision(40, 16));
-        System.out.println("Результат расчета №3 равен: " + differenceAndDivision(30, 20));
-        System.out.println("Результат расчета №4 равен: " + sumOfTwo(21.5, 8.5));
+        System.out.println("Результат расчета №3 равен: " + differenceAndDivision(30, 15));
+        System.out.println("Результат расчета №4 равен: " + sumOfTwo(21.5, 13.0));
+        System.out.println("Результат расчета №5 равено: " + all(230.0, 21.5, 13.0, 34.5));
 
     }
 }
