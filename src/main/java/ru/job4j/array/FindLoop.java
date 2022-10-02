591ad7dea0d34ea5e9a2f4eb5ff1fd5b718e0884
Andrey <AlpatoV_A_A@mail.ru>
2022-10-02 20:56:44 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -9,7 +9,18 @@ public class FindLoop {
                 break;
             }
         }
-        return rst;
+            return rst;
+    }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+            int rst = -1;
+            for (int index = start; index <= finish; index++) {
+                if (data[index] == el) {
+                    rst = index;
+                }
+            }
+            return rst;
     }
 }
+
 
--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -14,19 +14,32 @@ public class FindLoopTest {
     }
 
     @Test
-    public void whenArrayHas16Then1() {
-        int[] data = new int[] {8, 16, 5};
-        int el = 16;
-        int result = FindLoop.indexOf(data, el);
-        int expected = 1;
+    public void whenArrayHas2Then3() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void whenArrayHasNot10ThenMinus1() {
-        int[] data = new int[] {5, 8, 9};
-        int el = 10;
+    public void whenArrayHasNot8ThenMinus1() {
+        int[] data = new int[] {5, 7, 9};
+        int el = 8;
         int result = FindLoop.indexOf(data, el);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void notFound9() {
+        int[] data = new int[] {2, 4, 5, 7, 8};
+        int el = 8;
+        int start = 0;
+        int finish = 3;
+        int result = FindLoop.indexInRange(data, el, start, finish);
         int expected = -1;
         assertThat(result).isEqualTo(expected);
     }
