4c23ff6e79a4791f5be0bc877d0150f5d917beac
Andrey <AlpatoV_A_A@mail.ru>
2022-10-12 21:58:57 +0300

    6.7.3. Массив из диагонали матрицы

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -5,51 +5,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class MatrixCheckTest {
     @Test
-    public void whenHasMonoHorizontal() {
+    public void whenDiagonalFullX() {
         char[][] input = {
-                {' ', ' ', ' '},
-                {'X', 'X', 'X'},
-                {' ', ' ', ' '},
-        };
-        int row = 1;
-        boolean result = MatrixCheck.monoHorizontal(input, row);
-        assertThat(result).isTrue();
-    }
-
-    @Test
-    public void whenNoHasMonoHorizontal() {
-        char[][] input = {
-                {'X', 'X', ' '},
-                {' ', ' ', ' '},
-                {' ', ' ', ' '},
-        };
-        int row = 0;
-        boolean result = MatrixCheck.monoHorizontal(input, row);
-        assertThat(result).isFalse();
-    }
-
-    @Test
-    public void whenHasMonoVertical() {
-        char[][] input = {
-                {' ', ' ', 'X'},
-                {' ', ' ', 'X'},
-                {' ', ' ', 'X'},
-        };
-        int column = 2;
-        boolean result = MatrixCheck.monoVertical(input, column);
-        assertThat(result).isTrue();
-    }
-
-    @Test
-    public void whenNoHasMonoVertical() {
-        char[][] input = {
-                {' ', ' ', ' '},
+                {'X', ' ', ' '},
                 {' ', 'X', ' '},
-                {' ', ' ', ' '},
+                {' ', ' ', 'X'},
         };
-        int row = 0;
-        boolean result = MatrixCheck.monoVertical(input, row);
-        assertThat(result).isFalse();
+        char[] result = MatrixCheck.extractDiagonal(input);
+        char[] expected = {'X', 'X', 'X'};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenDiagonalFullOne() {
+        char[][] input = {
+                {'1', ' ', ' '},
+                {' ', '1', ' '},
+                {' ', ' ', '1'},
+        };
+        char[] result = MatrixCheck.extractDiagonal(input);
+        char[] expected = {'1', '1', '1'};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenDiagonalMix() {
+        char[][] input = {
+                {'X', ' ', ' '},
+                {' ', 'Y', ' '},
+                {' ', ' ', 'Z'},
+        };
+        char[] result = MatrixCheck.extractDiagonal(input);
+        char[] expected = {'X', 'Y', 'Z'};
+        assertThat(result).containsExactly(expected);
     }
 }
-
